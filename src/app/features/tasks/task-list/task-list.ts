8a51d94b5ed6117349ec0ac8b56f210e971b0b66
iftekhar.ahmed <iftekhar.ahmed@datasoft-bd.com>
2026-04-26 01:57:09 +0600

    Completed Phase 2: task manager UI with add, delete, toggle, and styling

--- src/app/features/tasks/task-list/task-list.ts
+++ src/app/features/tasks/task-list/task-list.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
-export class TaskList {}
+export class TaskList {
+
+    tasks = [
+    { id: 1, title: "Learn Angular", done: false },
+    { id: 2, title: "Build App", done: true }
+  ];
+
+  toggleTask(task: any) {
+    task.done = !task.done;
+  }
+
+ newTaskTitle = "";
+
+addTask() {
+  if (!this.newTaskTitle.trim()) return;
+
+  this.tasks.push({
+    id: this.tasks.length + 1,
+    title: this.newTaskTitle,
+    done: false
+  });
+
+  this.newTaskTitle = "";
+}
+
+
+deleteTask(id: number) {
+  this.tasks = this.tasks.filter(task => task.id !== id);
+}
+
+
+}
